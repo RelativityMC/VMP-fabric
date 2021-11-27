2b00d84c2d2b854cc8d042bc0ebc2db8122645c8
ishland <ishlandmc@yeah.net>
2021-11-27 22:01:16 +0800

    Optimize isTooFarFromPlayersToSpawnMobs

---
+++ src/main/java/com/ishland/vmp/mixins/playerwatching/optimize_nearby_player_lookups/MixinThreadedAnvilChunkStorage.java
@@ -0,0 +1,45 @@
+package com.ishland.vmp.mixins.playerwatching.optimize_nearby_player_lookups;
+
+import com.ishland.vmp.common.chunkwatching.AreaPlayerChunkWatchingManager;
+import net.minecraft.entity.Entity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.PlayerChunkWatchingManager;
+import net.minecraft.server.world.ThreadedAnvilChunkStorage;
+import net.minecraft.util.math.ChunkPos;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(ThreadedAnvilChunkStorage.class)
+public abstract class MixinThreadedAnvilChunkStorage {
+
+    @Shadow @Final private PlayerChunkWatchingManager playerChunkWatchingManager;
+
+    @Shadow @Final private ThreadedAnvilChunkStorage.TicketManager ticketManager;
+
+    @Shadow
+    private static double getSquaredDistance(ChunkPos pos, Entity entity) {
+        throw new AbstractMethodError();
+    }
+
+    /**
+     * @author ishland
+     * @reason optimize nearby player lookups
+     */
+    @Overwrite
+    public boolean isTooFarFromPlayersToSpawnMobs(ChunkPos chunkPos) {
+        long l = chunkPos.toLong();
+        if (!this.ticketManager.method_20800(l)) return true;
+        final AreaPlayerChunkWatchingManager chunkWatchingManager = (AreaPlayerChunkWatchingManager) this.playerChunkWatchingManager;
+        final Object[] array = chunkWatchingManager.getPlayersWatchingChunkArray(chunkPos.toLong());
+
+        for (Object __player : array) {
+            if (__player instanceof ServerPlayerEntity player) {
+                if (!player.isSpectator() && getSquaredDistance(chunkPos, player) < 16384.0) return false;
+            }
+        }
+        return true;
+    }
+
+}
