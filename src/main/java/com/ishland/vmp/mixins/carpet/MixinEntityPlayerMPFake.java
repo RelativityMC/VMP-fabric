73c493329a546abd8e2d944bc17cc5db1d8f9207
ishland <ishlandmc@yeah.net>
2021-11-28 11:40:24 +0800

    Some carpet opt

---
+++ src/main/java/com/ishland/vmp/mixins/carpet/MixinEntityPlayerMPFake.java
@@ -0,0 +1,40 @@
+package com.ishland.vmp.mixins.carpet;
+
+import com.mojang.authlib.GameProfile;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerChunkManager;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.Vec3d;
+import org.spongepowered.asm.mixin.Dynamic;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Pseudo
+@Mixin(targets = "carpet.patches.EntityPlayerMPFake")
+public abstract class MixinEntityPlayerMPFake extends ServerPlayerEntity {
+
+    public MixinEntityPlayerMPFake(MinecraftServer server, ServerWorld world, GameProfile profile) {
+        super(server, world, profile);
+    }
+
+    @Unique private double vmp_lastX = Double.NaN;
+    @Unique private double vmp_lastY = Double.NaN;
+    @Unique private double vmp_lastZ = Double.NaN;
+
+    @Dynamic
+    @Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerChunkManager;updatePosition(Lnet/minecraft/server/network/ServerPlayerEntity;)V"))
+    private void redirectUpdatePosition(ServerChunkManager serverChunkManager, ServerPlayerEntity __unused) {
+        final Vec3d pos = this.getPos();
+        if (pos.x != vmp_lastX || pos.y != vmp_lastY || pos.z != vmp_lastZ) { // only do update when position changes
+            vmp_lastX = pos.x;
+            vmp_lastY = pos.y;
+            vmp_lastZ = pos.z;
+            serverChunkManager.updatePosition(this);
+        }
+    }
+
+}
