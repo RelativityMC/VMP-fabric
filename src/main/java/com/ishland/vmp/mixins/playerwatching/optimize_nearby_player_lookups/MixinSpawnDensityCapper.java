6590aa8ecc1bc85dfd52921f8e45d7d9223abbb3
ishland <ishlandmc@yeah.net>
2021-12-04 19:40:20 +0800

    Replace spawn density hashmap with fastutil map

--- src/main/java/com/ishland/vmp/mixins/playerwatching/optimize_nearby_player_lookups/MixinSpawnDensityCapper.java
+++ src/main/java/com/ishland/vmp/mixins/playerwatching/optimize_nearby_player_lookups/MixinSpawnDensityCapper.java
@@ -2,6 +2,7 @@ package com.ishland.vmp.mixins.playerwatching.optimize_nearby_player_lookups;
 
 import com.ishland.vmp.common.chunkwatching.AreaPlayerChunkWatchingManager;
 import com.ishland.vmp.mixins.access.IThreadedAnvilChunkStorage;
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.PlayerChunkWatchingManager;
@@ -10,8 +11,12 @@ import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.SpawnDensityCapper;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Map;
 import java.util.function.Function;
@@ -19,9 +24,19 @@ import java.util.function.Function;
 @Mixin(SpawnDensityCapper.class)
 public class MixinSpawnDensityCapper {
 
-    @Shadow @Final private Map<ServerPlayerEntity, SpawnDensityCapper.DensityCap> playersToDensityCap;
-    @Shadow @Final private ThreadedAnvilChunkStorage threadedAnvilChunkStorage;
+    @Mutable
+    @Shadow
+    @Final
+    private Map<ServerPlayerEntity, SpawnDensityCapper.DensityCap> playersToDensityCap;
+    @Shadow
+    @Final
+    private ThreadedAnvilChunkStorage threadedAnvilChunkStorage;
     private static final Function<ServerPlayerEntity, SpawnDensityCapper.DensityCap> newDensityCap = ignored -> new SpawnDensityCapper.DensityCap();
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void onInit(CallbackInfo info) {
+        this.playersToDensityCap = new Object2ObjectOpenHashMap<>();
+    }
 
     /**
      * @author ishland
