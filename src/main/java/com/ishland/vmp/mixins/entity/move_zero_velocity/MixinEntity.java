5491c9a843ee3fc6d0589f612efa399199f8556a
ishland <ishlandmc@yeah.net>
2022-03-12 21:56:32 +0800

    Bring back `move_zero_velocity`

--- src/main/java/com/ishland/vmp/mixins/entity/move_zero_velocity/MixinEntity.java
+++ src/main/java/com/ishland/vmp/mixins/entity/move_zero_velocity/MixinEntity.java
@@ -2,8 +2,11 @@ package com.ishland.vmp.mixins.entity.move_zero_velocity;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.MovementType;
+import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -11,9 +14,21 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Entity.class)
 public class MixinEntity {
 
+    @Shadow private Box boundingBox;
+    @Unique
+    private boolean boundingBoxChanged = false;
+
     @Inject(method = "move", at = @At("HEAD"), cancellable = true)
     private void onMove(MovementType movementType, Vec3d movement, CallbackInfo ci) {
-//        if (movement.equals(Vec3d.ZERO)) ci.cancel();
+        if (!boundingBoxChanged && movement.equals(Vec3d.ZERO)) {
+            ci.cancel();
+            boundingBoxChanged = false;
+        }
+    }
+
+    @Inject(method = "setBoundingBox", at = @At("HEAD"))
+    private void onBoundingBoxChanged(Box boundingBox, CallbackInfo ci) {
+        if (!this.boundingBox.equals(boundingBox)) boundingBoxChanged = true;
     }
 
 }
