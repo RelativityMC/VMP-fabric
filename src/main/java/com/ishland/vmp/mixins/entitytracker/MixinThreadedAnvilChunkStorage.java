8aa53f36e7c235a772235a2d8b38068dd4f6d481
ishland <ishlandmc@yeah.net>
2022-03-12 23:03:49 +0800

    Use linked map for entity trackers for faster iteration

---
+++ src/main/java/com/ishland/vmp/mixins/entitytracker/MixinThreadedAnvilChunkStorage.java
@@ -0,0 +1,25 @@
+package com.ishland.vmp.mixins.entitytracker;
+
+import it.unimi.dsi.fastutil.ints.Int2ObjectLinkedOpenHashMap;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import net.minecraft.server.world.ThreadedAnvilChunkStorage;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ThreadedAnvilChunkStorage.class)
+public class MixinThreadedAnvilChunkStorage {
+
+    @Mutable
+    @Shadow @Final private Int2ObjectMap<ThreadedAnvilChunkStorage.EntityTracker> entityTrackers;
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void onInit(CallbackInfo info) {
+        this.entityTrackers = new Int2ObjectLinkedOpenHashMap<>();
+    }
+
+}
