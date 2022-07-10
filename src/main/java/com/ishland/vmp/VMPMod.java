c7cec88866d2d870d93796a9c8ced4cce57f4131
ishland <ishlandmc@yeah.net>
2022-07-11 07:44:22 +0800

    An attempt to fix prodlaunch

--- src/main/java/com/ishland/vmp/VMPMod.java
+++ src/main/java/com/ishland/vmp/VMPMod.java
@@ -1,11 +1,25 @@
 package com.ishland.vmp;
 
 import com.ishland.raknetify.fabric.common.connection.RakNetMultiChannel;
+import com.ishland.vmp.mixins.access.INetworkState;
+import com.ishland.vmp.mixins.access.INetworkStatePacketHandler;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.network.NetworkSide;
+import net.minecraft.network.NetworkState;
+import net.minecraft.network.Packet;
+
+import java.util.Map;
 
 public class VMPMod implements ModInitializer {
     @Override
     public void onInitialize() {
         RakNetMultiChannel.init();
+
+        for (Map.Entry<NetworkSide, ? extends NetworkState.PacketHandler<?>> entry : ((INetworkState) (Object) NetworkState.PLAY).getPacketHandlers().entrySet()) {
+            for (Object2IntMap.Entry<Class<? extends Packet<?>>> type : ((INetworkStatePacketHandler) entry.getValue()).getPacketIds().object2IntEntrySet()) {
+                RakNetMultiChannel.getPacketChannelOverride(type.getKey());
+            }
+        }
     }
 }
