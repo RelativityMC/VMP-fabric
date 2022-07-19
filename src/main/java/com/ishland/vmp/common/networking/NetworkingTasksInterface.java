0ab420cd12cb4ca8f17247bdec5c61e0a97af0a7
ishland <ishlandmc@yeah.net>
2022-07-19 23:19:31 +0800

    Remove unnecessary networking task queue

--- src/main/java/com/ishland/vmp/common/networking/NetworkingTasksInterface.java
+++
@@ -1,9 +0,0 @@
-package com.ishland.vmp.common.networking;
-
-import net.minecraft.util.thread.ThreadExecutor;
-
-public interface NetworkingTasksInterface {
-
-    ThreadExecutor<Runnable> getNetworkingTasksExecutor();
-
-}
