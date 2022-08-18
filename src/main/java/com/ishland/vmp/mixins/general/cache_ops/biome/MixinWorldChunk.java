df7314aa643bd012dd09bc09cb15e22d905e6e72
ishland <ishlandmc@yeah.net>
2022-08-18 13:25:20 +0800

    Lower memory usage of biome cache

--- src/main/java/com/ishland/vmp/mixins/general/cache_ops/biome/MixinWorldChunk.java
+++ src/main/java/com/ishland/vmp/mixins/general/cache_ops/biome/MixinWorldChunk.java
@@ -8,20 +8,27 @@ import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.world.ChunkRegion;
 import net.minecraft.world.HeightLimitView;
+import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeKeys;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.ChunkSection;
+import net.minecraft.world.chunk.PalettedContainer;
 import net.minecraft.world.chunk.UpgradeData;
 import net.minecraft.world.chunk.WorldChunk;
 import net.minecraft.world.gen.chunk.BlendingData;
 import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 
 import java.util.concurrent.CompletableFuture;
 
 @Mixin(WorldChunk.class)
 public abstract class MixinWorldChunk extends Chunk implements PreloadingBiome {
+
+    @Shadow @Final private World world;
 
     public MixinWorldChunk(ChunkPos pos, UpgradeData upgradeData, HeightLimitView heightLimitView, Registry<Biome> biome, long inhabitedTime, @Nullable ChunkSection[] sectionArrayInitializer, @Nullable BlendingData blendingData) {
         super(pos, upgradeData, heightLimitView, biome, inhabitedTime, sectionArrayInitializer, blendingData);
@@ -34,19 +41,29 @@ public abstract class MixinWorldChunk extends Chunk implements PreloadingBiome {
     @Unique
     private CompletableFuture<Void> vmp$preloadBiomeFuture = null;
     @Unique
-    private RegistryEntry<Biome>[][] vmp$preloadedBiome = null; // y, xz(indexed)
+    private PalettedContainer<RegistryEntry<Biome>>[] vmp$preloadedBiome = null; // y, xz(indexed)
 
     @Override
     public void vmp$tryPreloadBiome(ChunkRegion chunkRegion) {
         synchronized (this) {
             if (vmp$preloadBiomeFuture == null) {
                 vmp$preloadBiomeFuture = CompletableFuture.runAsync(() -> {
-                    RegistryEntry<Biome>[][] preloadedBiome = new RegistryEntry[this.getHeight()][16 * 16];
+                    PalettedContainer<RegistryEntry<Biome>>[] preloadedBiome = new PalettedContainer[this.countVerticalSections()];
+                    final Registry<Biome> biomeRegistry = this.world.getRegistryManager().get(Registry.BIOME_KEY);
+                    for (int y = 0; y < preloadedBiome.length; y++) {
+                        preloadedBiome[y] = new PalettedContainer<>(
+                                biomeRegistry.getIndexedEntries(),
+                                biomeRegistry.entryOf(BiomeKeys.PLAINS),
+                                PalettedContainer.PaletteProvider.BLOCK_STATE
+                        );
+                    }
                     for (int x = 0; x < 16; x++) {
                         for (int z = 0; z < 16; z++) {
-                            for (int y = 0; y < this.getHeight(); y++) {
-                                preloadedBiome[y][vmp$getBiomeIndex(x, z)]
-                                        = chunkRegion.getBiome(new BlockPos(this.pos.getStartX() + x, this.getBottomY() + y, this.pos.getStartZ() + z));
+                            for (int y = this.getBottomY(); y < this.getTopY(); y++) {
+                                preloadedBiome[this.getSectionIndex(y)].set(
+                                        x & 15,y & 15,z & 15,
+                                        chunkRegion.getBiome(new BlockPos(this.pos.getStartX() + x, y, this.pos.getStartZ() + z))
+                                );
                             }
                         }
                     }
@@ -61,7 +78,7 @@ public abstract class MixinWorldChunk extends Chunk implements PreloadingBiome {
         if (vmp$preloadedBiome == null) {
             return null;
         }
-        final int yIndex = MathHelper.clamp(y + getBottomY(), 0, this.getHeight() - 1);
-        return vmp$preloadedBiome[yIndex][vmp$getBiomeIndex(x, z)];
+        final int yIndex = MathHelper.clamp(this.getSectionIndex(y), 0, this.vmp$preloadedBiome.length - 1);
+        return vmp$preloadedBiome[yIndex].get(x & 15, y & 15, z & 15);
     }
 }
