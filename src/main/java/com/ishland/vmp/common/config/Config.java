023a02f71c25fc707b498bc902875d1c7970df11
ishland <ishlandmc@yeah.net>
2022-08-28 18:12:37 +0800

    Copy-on-write config to purge old entries

--- src/main/java/com/ishland/vmp/common/config/Config.java
+++ src/main/java/com/ishland/vmp/common/config/Config.java
@@ -21,6 +21,7 @@ public class Config {
 
     static {
         final Properties properties = new Properties();
+        final Properties newProperties = new Properties();
         final Path path = FabricLoader.getInstance().getConfigDir().resolve("vmp.properties");
         if (Files.isRegularFile(path)) {
             try (InputStream in = Files.newInputStream(path, StandardOpenOption.CREATE)) {
@@ -29,14 +30,14 @@ public class Config {
                 throw new RuntimeException(e);
             }
         }
-        TARGET_CHUNK_SEND_RATE = getInt(properties, "target_chunk_send_rate", -1);
-        USE_PACKET_PRIORITY_SYSTEM = getBoolean(properties, "use_packet_priority_system", true);
-        USE_ASYNC_LOGGING = getBoolean(properties, "use_async_logging", true);
-        USE_OPTIMIZED_ENTITY_TRACKING = getBoolean(properties, "use_optimized_entity_tracking", true);
-        USE_MULTIPLE_NETTY_EVENT_LOOPS = getBoolean(properties, "use_multiple_netty_event_loops", true);
-        USE_ASYNC_PORTALS = getBoolean(properties, "use_async_portals", true);
+        TARGET_CHUNK_SEND_RATE = getInt(properties, newProperties, "target_chunk_send_rate", -1);
+        USE_PACKET_PRIORITY_SYSTEM = getBoolean(properties, newProperties, "use_packet_priority_system", true);
+        USE_ASYNC_LOGGING = getBoolean(properties, newProperties, "use_async_logging", true);
+        USE_OPTIMIZED_ENTITY_TRACKING = getBoolean(properties, newProperties, "use_optimized_entity_tracking", true);
+        USE_MULTIPLE_NETTY_EVENT_LOOPS = getBoolean(properties, newProperties, "use_multiple_netty_event_loops", true);
+        USE_ASYNC_PORTALS = getBoolean(properties, newProperties, "use_async_portals", true);
         try (OutputStream out = Files.newOutputStream(path, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)) {
-            properties.store(out, "Configuration file for VMP");
+            newProperties.store(out, "Configuration file for VMP");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -45,20 +46,24 @@ public class Config {
     public static void init() {
     }
 
-    private static int getInt(Properties properties, String key, int def) {
+    private static int getInt(Properties properties, Properties newProperties, String key, int def) {
         try {
-            return Integer.parseInt(properties.getProperty(key));
+            final int i = Integer.parseInt(properties.getProperty(key));
+            newProperties.setProperty(key, String.valueOf(i));
+            return i;
         } catch (NumberFormatException e) {
-            properties.setProperty(key, String.valueOf(def));
+            newProperties.setProperty(key, String.valueOf(def));
             return def;
         }
     }
 
-    private static boolean getBoolean(Properties properties, String key, boolean def) {
+    private static boolean getBoolean(Properties properties, Properties newProperties, String key, boolean def) {
         try {
-            return parseBoolean(properties.getProperty(key));
+            final boolean b = parseBoolean(properties.getProperty(key));
+            newProperties.setProperty(key, String.valueOf(b));
+            return b;
         } catch (NumberFormatException e) {
-            properties.setProperty(key, String.valueOf(def));
+            newProperties.setProperty(key, String.valueOf(def));
             return def;
         }
     }
