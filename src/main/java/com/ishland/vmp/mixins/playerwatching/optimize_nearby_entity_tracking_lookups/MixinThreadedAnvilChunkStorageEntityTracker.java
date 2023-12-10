7af5834501f31e123ca5ab49f7a34170ead40565
ishland <ishlandmc@yeah.net>
2023-12-10 10:39:52 +0800

    fix: skip chunk sent check for optimized entity tracking
    Fixes #52

--- src/main/java/com/ishland/vmp/mixins/playerwatching/optimize_nearby_entity_tracking_lookups/MixinThreadedAnvilChunkStorageEntityTracker.java
+++ src/main/java/com/ishland/vmp/mixins/playerwatching/optimize_nearby_entity_tracking_lookups/MixinThreadedAnvilChunkStorageEntityTracker.java
@@ -18,6 +18,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
@@ -104,6 +105,11 @@ public abstract class MixinThreadedAnvilChunkStorageEntityTracker implements Ent
         if (this.listeners.isEmpty()) {
             ((EntityTrackerEntryExtension) this.entry).vmp$tickAlways();
         }
+    }
+
+    @Redirect(method = "updateTrackedStatus(Lnet/minecraft/server/network/ServerPlayerEntity;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ThreadedAnvilChunkStorage;isTracked(Lnet/minecraft/server/network/ServerPlayerEntity;II)Z"))
+    private boolean assumeAlwaysTracked(ThreadedAnvilChunkStorage instance, ServerPlayerEntity player, int chunkX, int chunkZ) {
+        return true;
     }
 
 }
