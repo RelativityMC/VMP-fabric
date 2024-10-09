8bbc7fffdb0ba6407f46839bc93fb2f39a131b9f
ishland <ishlandmc@yeah.net>
2024-10-09 16:22:39 +0800

    build: 1.21.2-pre1

--- src/main/java/com/ishland/vmp/mixins/playerwatching/optimize_nearby_player_lookups/MixinServerWorld.java
+++ src/main/java/com/ishland/vmp/mixins/playerwatching/optimize_nearby_player_lookups/MixinServerWorld.java
@@ -2,7 +2,6 @@ package com.ishland.vmp.mixins.playerwatching.optimize_nearby_player_lookups;
 
 import com.ishland.vmp.common.chunkwatching.AreaPlayerChunkWatchingManager;
 import com.ishland.vmp.common.playerwatching.TACSExtension;
-import com.ishland.vmp.mixins.access.IThreadedAnvilChunkStorage;
 import io.papermc.paper.util.MCUtil;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
@@ -16,7 +15,6 @@ import net.minecraft.server.world.ServerChunkLoadingManager;
 import net.minecraft.server.world.ServerChunkManager;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.ChunkSectionPos;
-import net.minecraft.util.profiler.Profiler;
 import net.minecraft.world.MutableWorldProperties;
 import net.minecraft.world.StructureWorldAccess;
 import net.minecraft.world.World;
@@ -25,9 +23,7 @@ import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
-import java.util.Set;
 import java.util.function.Predicate;
-import java.util.function.Supplier;
 
 @Mixin(ServerWorld.class)
 public abstract class MixinServerWorld extends World implements StructureWorldAccess {
@@ -66,53 +62,6 @@ public abstract class MixinServerWorld extends World implements StructureWorldAc
         }
 
         return nearestPlayer;
-    }
-
-    @Nullable
-    @Override
-    public PlayerEntity getClosestPlayer(TargetPredicate targetPredicate, LivingEntity entity) {
-        return this.getClosestPlayer(targetPredicate, entity, entity.getX(), entity.getY(), entity.getZ());
-    }
-
-    @Nullable
-    @Override
-    public PlayerEntity getClosestPlayer(TargetPredicate targetPredicate, LivingEntity entity, double x, double y, double z) {
-        final Object[] playersWatchingChunkArray = getPlayersWatchingChunkArray(x, z);
-
-        ServerPlayerEntity nearestPlayer = null;
-        double nearestDistance = Double.MAX_VALUE;
-        for (Object __player : playersWatchingChunkArray) {
-            if (__player instanceof ServerPlayerEntity player) {
-                if (targetPredicate == null || targetPredicate.test(entity, player)) {
-                    final double distance = player.squaredDistanceTo(x, y, z);
-                    if (distance < nearestDistance) {
-                        nearestDistance = distance;
-                        nearestPlayer = player;
-                    }
-                }
-            }
-        }
-
-        return nearestPlayer;
-    }
-
-    private Object[] getPlayersWatchingChunkArray(double x, double z) {
-        final ServerChunkLoadingManager threadedAnvilChunkStorage = this.getChunkManager().chunkLoadingManager;
-        final AreaPlayerChunkWatchingManager playerChunkWatchingManager = ((TACSExtension) threadedAnvilChunkStorage).getAreaPlayerChunkWatchingManager();
-        final int chunkX = ChunkSectionPos.getSectionCoord(x);
-        final int chunkZ = ChunkSectionPos.getSectionCoord(z);
-
-        // no maxDistance here so just search within the range,
-        // and hopefully it works
-
-        final Object[] playersWatchingChunkArray = playerChunkWatchingManager.getPlayersInGeneralAreaMap(MCUtil.getCoordinateKey(chunkX, chunkZ));
-        return playersWatchingChunkArray;
-    }
-
-    @Nullable
-    @Override
-    public PlayerEntity getClosestPlayer(TargetPredicate targetPredicate, double x, double y, double z) {
-        return this.getClosestPlayer(targetPredicate, null, x, y, z);
     }
 
     @Override
